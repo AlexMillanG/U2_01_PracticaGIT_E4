72b8c6cc350819a34dee566621cc7871249b414e
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2025-05-20 15:53:41 -0600

    primer commit

---
+++ servidor/src/main/java/mx/edu/utez/U2_01_PracticaGIT_E4/U201PracticaGitE4Application.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.U2_01_PracticaGIT_E4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U201PracticaGitE4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U201PracticaGitE4Application.class, args);
+	}
+
+}
---
+++ servidor/src/test/java/mx/edu/utez/U2_01_PracticaGIT_E4/U201PracticaGitE4ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.U2_01_PracticaGIT_E4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U201PracticaGitE4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
