16db817ad4b68edb158a3520e46f5468784fd484
NONAMEB-312 <133677796+NONAMEB-312@users.noreply.github.com>
2025-05-20 16:38:13 -0600

    entity

--- servidor/src/main/java/mx/edu/utez/U2_01_PracticaGIT_E4/models/provider/ProviderEntity.java
+++ servidor/src/main/java/mx/edu/utez/U2_01_PracticaGIT_E4/models/provider/ProviderEntity.java
@@ -4,18 +4,14 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 
 @Entity
 @Table(name = "provider")
 public class ProviderEntity {
     //nombre completo telefono correo
-    
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(length = 100, nullable = false)
