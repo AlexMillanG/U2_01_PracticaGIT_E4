cdff1f1dfba413e1c30e655b28cc0984057649d8
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2025-05-20 17:24:16 -0600

    Update ApiResponse.java

--- servidor/src/main/java/mx/edu/utez/U2_01_PracticaGIT_E4/config/ApiResponse.java
+++ servidor/src/main/java/mx/edu/utez/U2_01_PracticaGIT_E4/config/ApiResponse.java
@@ -2,14 +2,14 @@ package mx.edu.utez.U2_01_PracticaGIT_E4.config;
 
 import org.springframework.http.HttpStatus;
 
-public class ResponseEntity {
+public class ApiResponse {
 
     private Object data;
     private HttpStatus status;
     private Boolean error;
 
 
-    public ResponseEntity(Object data, HttpStatus status, Boolean error) {
+    public ApiResponse(Object data, HttpStatus status, Boolean error) {
         this.data = data;
         this.status = status;
         this.error = error;
