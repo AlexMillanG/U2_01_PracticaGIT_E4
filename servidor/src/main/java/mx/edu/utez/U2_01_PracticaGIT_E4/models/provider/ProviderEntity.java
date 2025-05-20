4198cbabb02d44ca2421a28a18ec27499bf2f387
NONAMEB-312 <133677796+NONAMEB-312@users.noreply.github.com>
2025-05-20 16:46:07 -0600

    entityall

--- servidor/src/main/java/mx/edu/utez/U2_01_PracticaGIT_E4/models/provider/ProviderEntity.java
+++ servidor/src/main/java/mx/edu/utez/U2_01_PracticaGIT_E4/models/provider/ProviderEntity.java
@@ -30,8 +30,60 @@ public class ProviderEntity {
     }
 
     public ProviderEntity(Long id, String name, String surname, String lastname, String phoneNumber, String email){
-        
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.lastname = lastname;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
     
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+    
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+    
+    public void setLastname(String lastname){
+        this.lastname = lastname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+
 }
