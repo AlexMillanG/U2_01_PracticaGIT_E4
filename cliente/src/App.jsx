89bc2e9009e7f06a6bd32aa8266c068cc360392a
EmmaSA1 <132949371+EmmaSA1@users.noreply.github.com>
2025-05-20 17:26:39 -0600

    Creacion de navbar y rutas

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
+import Navbar from './shared/Navbar'
+import { Proveedor } from './pages/compPrueba/Proveedor'
+import { Automoviles } from './pages/compPrueba/Automoviles'
 
 function App() {
 
   return (
     <>
-    <h1>hola</h1>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/proveedores" element={<Proveedor />} />
+      <Route path="/automoviles" element={<Automoviles />} />
+
       {/* <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} /> */}
     </Routes>
---
+++ cliente/src/pages/compPrueba/Automoviles.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Automoviles = () => {
+  return (
+    <div>Automoviles</div>
+  )
+}
