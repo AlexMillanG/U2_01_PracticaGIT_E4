5b7225fedbc7d93b2547ed172728241f05cda26a
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2025-05-20 16:32:15 -0600

    Update CarEntity.java

--- servidor/src/main/java/mx/edu/utez/U2_01_PracticaGIT_E4/models/car/CarEntity.java
+++ servidor/src/main/java/mx/edu/utez/U2_01_PracticaGIT_E4/models/car/CarEntity.java
@@ -4,15 +4,23 @@ import jakarta.persistence.*;
 import mx.edu.utez.U2_01_PracticaGIT_E4.models.provider.ProviderEntity;
 
 @Entity
-@Table(name="cars")
+@Table(name = "cars")
 public class CarEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String model;
     private String brand;
     private String plate;
+
+    @ManyToOne
+    @JoinColumn(name = "proveedor_id")
+    private ProviderEntity provider;
+
+    public CarEntity() {
+    }
 
     public CarEntity(Long id, String model, String brand, String plate) {
         this.id = id;
@@ -21,7 +29,45 @@ public class CarEntity {
         this.plate = plate;
     }
 
-    public CarEntity() {
+    // Getters y setters
+
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public ProviderEntity getProvider() {
+        return provider;
+    }
+
+    public void setProvider(ProviderEntity provider) {
+        this.provider = provider;
+    }
 }
