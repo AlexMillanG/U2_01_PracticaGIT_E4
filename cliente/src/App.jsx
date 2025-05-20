6c011ab1107b79d88ea77f24cc3fa1a45c0ef3ba
EmilianoCH <169091450+AldrickCHIB@users.noreply.github.com>
2025-05-20 17:37:15 -0600

    Conflicto resuelto en App.jsx tras aplicar stash

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -4,6 +4,7 @@ import { Home } from './pages/Home'
 import Navbar from './shared/Navbar'
 import { Proveedor } from './pages/compPrueba/Proveedor'
 import { Automoviles } from './pages/compPrueba/Automoviles'
+import { Cars } from './pages/cars/Cars'
 
 function App() {
 
@@ -12,11 +13,15 @@ function App() {
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
+
       <Route path="/proveedores" element={<Proveedor />} />
       <Route path="/automoviles" element={<Automoviles />} />
 
       {/* <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} /> */}
+       <Route path="/cars" element={<Cars />} />
+
+       {/* <Route path="/contact" element={<Contact />} />  */}
     </Routes>
     </>
   )
